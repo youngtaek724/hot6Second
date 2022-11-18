0aba7eb8430c96bcf03087bed1f1cac32c62a41b
kimhyobbbb <111670808+kimhyobbbb@users.noreply.github.com>
2022-11-18 22:05:02 +0900

    이거가능

---
+++ src/test/java/com/hot6/web/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hot6.web.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
