e4290d792ba34ed8da6628e729831183e4115d5c
Kojinhyeok <jinhyeokko12@gmail.com>
2022-11-25 18:36:40 +0900

    백엔드 툴 중간 수정

---
+++ src/main/java/com/hot6/web/spring/mapper/RankingMapper.java
@@ -0,0 +1,25 @@
+package com.hot6.web.spring.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ReplyMapper {
+//   댓글 추가
+//    public void insert(ReplyVO replyVO);
+
+//    댓글 수정
+//    public void update(ReplyVO replyVO);
+
+//    댓글 삭제
+//    public void delete(Long replyNumber);
+
+//    댓글 조회
+//    public ReplyVO select(Long replyNumber);
+
+//    댓글 전체 조회
+//    public List<ReplyVO> selectAll(Long boardNumber);
+
+}
---
+++ src/main/java/com/hot6/web/spring/service/UserService.java
@@ -0,0 +1,11 @@
+package com.hot6.web.spring.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyPageService {
+
+}
