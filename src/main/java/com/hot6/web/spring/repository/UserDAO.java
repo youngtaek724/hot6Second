19c0b4fb0476f958dcbdb15915c0131b7aff5c18
kimhyobbbb <111670808+kimhyobbbb@users.noreply.github.com>
2022-11-27 15:18:33 +0900

    loginPage 1차 수정
    login, join Controller 통한 db 조회

--- src/main/java/com/hot6/web/spring/repository/UserDAO.java
+++ src/main/java/com/hot6/web/spring/repository/UserDAO.java
@@ -1,6 +1,7 @@
 package com.hot6.web.spring.repository;
 
 
+import com.hot6.web.spring.domain.vo.UserVO;
 import com.hot6.web.spring.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,14 @@ import org.springframework.stereotype.Repository;
 public class UserDAO {
     private final UserMapper userMapper;
 // 추가 save
+    // 일반
+    public void saveUser(UserVO userVO){
+        userMapper.insertUser(userVO);
+    }
+    // 기관
+    public void saveCompany(UserVO userVO){
+        userMapper.insertCompany(userVO);
+    }
 
 // 수정 set
 
@@ -20,6 +29,9 @@ public class UserDAO {
 // 전체 조회 findByIdAll
 
 // 개수 findCountAll
+
+    // 로그인 시 아이디 조회
+    public int checkUser(UserVO userVO){return userMapper.selectUser(userVO);}
 }
 
 
--- src/main/java/com/hot6/web/spring/service/UserService.java
+++ src/main/java/com/hot6/web/spring/service/UserService.java
@@ -1,13 +1,26 @@
 package com.hot6.web.spring.service;
 
 
+import com.hot6.web.spring.domain.vo.UserVO;
+import com.hot6.web.spring.repository.UserDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    // 회원가입(유저 추가)
+    private final UserDAO userDAO;
+
+    // 회원가입
+    // 일반
+    public void saveUser(UserVO userVO){
+        userDAO.saveUser(userVO);
+    }
+    // 기관
+    public void saveCompany(UserVO userVO){
+        userDAO.saveCompany(userVO);
+    }
+
 
     // 내 정보 수정(유저 수정)
 
@@ -15,4 +28,8 @@ public class UserService {
 
     // 내 정보 보기(유저 조회)
 
+    // 로그인 시 아이디 조회
+    public int checkUser(UserVO userVO){
+        return userDAO.checkUser(userVO);
+    }
 }
