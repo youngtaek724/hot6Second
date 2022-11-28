11bc9b3cc3186394474bcb6c9acdb6905a2aaf55
kimhyobbbb <111670808+kimhyobbbb@users.noreply.github.com>
2022-11-28 18:29:48 +0900

    insert 유저 정보 추가
    grade, type 정보 추가 가능
    아이디 틀렸을 때 문구 설정 추가

--- src/main/java/com/hot6/web/spring/controller/MypageController.java
+++ src/main/java/com/hot6/web/spring/controller/MypageController.java
@@ -2,11 +2,36 @@ package com.hot6.web.spring.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/mypage/*")
 public class MypageController {
-    // 문의하기
+    // 페이지 이동
+    @GetMapping("/myPageMain")
+    public String pageMain(){
+        return "myPage/myPageMain";
+    }
+
+    @GetMapping("/myPageInquary")
+    public String pageInquiry(){
+        return "myPage/myPageInquary";
+    }
+
+    @GetMapping("/myPageQuizNo")
+    public String pageQuizNo(){
+        return "myPage/myPageQuizNo";
+    }
+
+    @GetMapping("/myPageQuizOk")
+    public String pageQuizOk(){
+        return "myPage/myPageQuizOk";
+    }
+
+
+
 
     // 탈퇴
 
